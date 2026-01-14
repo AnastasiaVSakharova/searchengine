f94a3d87a62e965ea4ee1666de320b0175a062b2
Сахарова Анастасия <anastasiya.sakharova@gazprombank.ru>
2026-01-14 15:28:04 +0300

    Для Datasearch убраны конструкторы

--- src/main/java/searchengine/dto/DataSearch.java
+++ src/main/java/searchengine/dto/DataSearch.java
@@ -1,10 +1,9 @@
 package searchengine.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
 public class DataSearch {
     private String site;
     private String siteName;
@@ -12,24 +11,4 @@ public class DataSearch {
     private String title;
     private String snippet;
     private double relevance;
-
-    // конструктор копирования
-    public DataSearch(DataSearch data) {
-        this.site = data.getSite();
-        this.siteName = data.getSiteName();
-        this.uri = data.getUri();
-        this.title = data.getTitle();
-        this.snippet = data.getSnippet();
-        this.relevance = data.getRelevance();
-    }
-
-    public DataSearch(String site, String siteName, String uri, String title,  String snippet, double relevance) {
-        this.relevance = relevance;
-        this.site = site;
-        this.siteName = siteName;
-        this.snippet = snippet;
-        this.title = title;
-        this.uri = uri;
-    }
-
 }
